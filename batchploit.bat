4cc1a3fa24389b32df69a01edacfa3d94a046bb2
iamSHINIGAMI <71846446+iamSHINIGAMI@users.noreply.github.com>
2020-12-05 16:54:47 +0100

    Version A1.0.0 of B4TCHPL01T
     ▄▄▄▄    ▄▄▄     ▄▄▄█████▓ ▄████▄   ██░ ██  ██▓███   ██▓     ▒█████   ██▓▄▄▄█████▓
    ▓█████▄ ▒████▄   ▓  ██▒ ▓▒▒██▀ ▀█  ▓██░ ██▒▓██░  ██▒▓██▒    ▒██▒  ██▒▓██▒▓  ██▒ ▓▒
    ▒██▒ ▄██▒██  ▀█▄ ▒ ▓██░ ▒░▒▓█    ▄ ▒██▀▀██░▓██░ ██▓▒▒██░    ▒██░  ██▒▒██▒▒ ▓██░ ▒░
    ▒██░█▀  ░██▄▄▄▄██░ ▓██▓ ░ ▒▓▓▄ ▄██▒░▓█ ░██ ▒██▄█▓▒ ▒▒██░    ▒██   ██░░██░░ ▓██▓ ░ 
    ░▓█  ▀█▓ ▓█   ▓██▒ ▒██▒ ░ ▒ ▓███▀ ░░▓█▒░██▓▒██▒ ░  ░░██████▒░ ████▓▒░░██░  ▒██▒ ░ 
    ░▒▓███▀▒ ▒▒   ▓▒█░ ▒ ░░   ░ ░▒ ▒  ░ ▒ ░░▒░▒▒▓▒░ ░  ░░ ▒░▓  ░░ ▒░▒░▒░ ░▓    ▒ ░░   
    ▒░▒   ░   ▒   ▒▒ ░   ░      ░  ▒    ▒ ░▒░ ░░▒ ░     ░ ░ ▒  ░  ░ ▒ ▒░  ▒ ░    ░    
     ░    ░   ░   ▒    ░      ░         ░  ░░ ░░░         ░ ░   ░ ░ ░ ▒   ▒ ░  ░      
     ░            ░  ░        ░ ░       ░  ░  ░             ░  ░    ░ ░   ░           
          ░                   ░

---
+++ batchploit.bat
@@ -0,0 +1,89 @@
+@echo off
+title B4TCHPL01T - by SHINIGAMI
+echo [SHINIGAMI] I know that it isnt an exploit, but it is a "backdoor" , yea i dont know which words i should use , but i spend 3 Hours on making this Program going into the PRE-Alpha!!! And i´m very proud of myself for this.
+echo [SHINIGAMI] And yes, i ONLY host this Program on github, you cant find it somewhere else! ( You can, but only if some B4st4rd uploaded it there. )
+echo [NOTE] You CANT use programs such as diskpart , because they need administrative rights!
+echo [NOTE] Press any key to get to set up B4TCHPL01T
+pause > nul
+
+color 0a
+echo.
+echo.
+echo.
+echo [INFO] SETTING B4TCHPL01T UP -__
+echo %random%%random%%random%
+timeout 1 > nul
+cls
+color 0b
+echo.
+echo.
+echo.
+echo [INFO] SETTING B4TCHPL01T UP _-_
+echo %random%%random%%random%
+timeout 1 > nul
+cls
+color 0c
+echo.
+echo.
+echo.
+echo [INFO] SETTING B4TCHPL01T UP __-
+echo %random%%random%%random%
+timeout 1 > nul
+cls
+color 0a
+echo.
+echo.
+echo.
+echo [INFO] SETTING B4TCHPL01T UP -__
+echo %random%%random%%random%
+timeout 1 > nul
+cls
+color 0b
+echo.
+echo.
+echo.
+echo [INFO] SETTING B4TCHPL01T UP _-_
+echo %random%%random%%random%
+timeout 1 > nul
+cls
+color 0c
+echo.
+echo.
+echo.
+echo [INFO] SETTING B4TCHPL01T UP __-
+echo %random%%random%%random%
+timeout 1 > nul
+
+
+echo [SUCCESS] !SETTED UP B4TCHPL01T!
+timeout 2 > nul
+echo [INFO] Creating your SID File, please wait
+timeout 1 > nul
+cls
+echo GENERATING :.::.:
+timeout 1 > nul 
+cls
+echo GENERATING ::..::
+timeout 1 > nul 
+cls
+echo GENERATING .::::.
+timeout 1 > nul 
+echo [SUCCESS] 
+cls
+echo "[BPL01TSID] Your B4TCHPL01T SID: %random%_%random%_B4TCHPL01T_%random%" > B4TCHPL01TSID.txt
+cls
+echo  ▄▄▄▄    ▄▄▄     ▄▄▄█████▓ ▄████▄   ██░ ██  ██▓███   ██▓     ▒█████   ██▓▄▄▄█████▓
+echo ▓█████▄ ▒████▄   ▓  ██▒ ▓▒▒██▀ ▀█  ▓██░ ██▒▓██░  ██▒▓██▒    ▒██▒  ██▒▓██▒▓  ██▒ ▓▒
+echo ▒██▒ ▄██▒██  ▀█▄ ▒ ▓██░ ▒░▒▓█    ▄ ▒██▀▀██░▓██░ ██▓▒▒██░    ▒██░  ██▒▒██▒▒ ▓██░ ▒░
+echo ▒██░█▀  ░██▄▄▄▄██░ ▓██▓ ░ ▒▓▓▄ ▄██▒░▓█ ░██ ▒██▄█▓▒ ▒▒██░    ▒██   ██░░██░░ ▓██▓ ░ 
+echo ░▓█  ▀█▓ ▓█   ▓██▒ ▒██▒ ░ ▒ ▓███▀ ░░▓█▒░██▓▒██▒ ░  ░░██████▒░ ████▓▒░░██░  ▒██▒ ░ 
+echo ░▒▓███▀▒ ▒▒   ▓▒█░ ▒ ░░   ░ ░▒ ▒  ░ ▒ ░░▒░▒▒▓▒░ ░  ░░ ▒░▓  ░░ ▒░▒░▒░ ░▓    ▒ ░░   
+echo ▒░▒   ░   ▒   ▒▒ ░   ░      ░  ▒    ▒ ░▒░ ░░▒ ░     ░ ░ ▒  ░  ░ ▒ ▒░  ▒ ░    ░    
+echo  ░    ░   ░   ▒    ░      ░         ░  ░░ ░░░         ░ ░   ░ ░ ░ ▒   ▒ ░  ░      
+echo  ░            ░  ░        ░ ░       ░  ░  ░             ░  ░    ░ ░   ░           
+echo       ░                   ░                                                         
+
+echo.
+echo. 
+set /p cmd=B4TCHPL01T:
+%cmd%                                                          
