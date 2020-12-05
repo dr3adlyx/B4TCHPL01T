134320f9fc77a6d6819877c6f6fb43db07757b87
iamSHINIGAMI <71846446+iamSHINIGAMI@users.noreply.github.com>
2020-12-05 23:38:53 +0100

    ALPHA 1!!!
    We finally reached A1.0.0, this shit took seven hours! 
    # SEVEN
    Hours...

--- batchploit.bat
+++ batchploit.bat
@@ -1,11 +1,18 @@
 @echo off
 title B4TCHPL01T - by SHINIGAMI
-echo [SHINIGAMI] I know that it isnt an exploit, but it is a "backdoor" , yea i dont know which words i should use , but i spend 3 Hours on making this Program going into the PRE-Alpha!!! And i´m very proud of myself for this.
+chcp 65001 > nul
+echo ██████╗   ██╗██╗████████╗ █████╗ ██╗  ██╗██████╗ ██╗      █████╗   ███╗  ████████╗
+echo ██╔══██╗ ██╔╝██║╚══██╔══╝██╔══██╗██║  ██║██╔══██╗██║     ██╔══██╗ ████║  ╚══██╔══╝
+echo ██████╦╝██╔╝ ██║   ██║   ██║  ╚═╝███████║██████╔╝██║     ██║  ██║██╔██║     ██║   
+echo ██╔══██╗███████║   ██║   ██║  ██╗██╔══██║██╔═══╝ ██║     ██║  ██║╚═╝██║     ██║   
+echo ██████╦╝╚════██║   ██║   ╚█████╔╝██║  ██║██║     ███████╗╚█████╔╝███████╗   ██║   
+echo ╚═════╝      ╚═╝   ╚═╝    ╚════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝ ╚════╝ ╚══════╝   ╚═╝   
+echo [SHINIGAMI] I know that it isnt an exploit, but it is a "backdoor" , yea i dont know which words i should use , but i spend 3 Hours on making this Program going into the PRE-Alpha!!! And imm very proud of myself for this.
 echo [SHINIGAMI] And yes, i ONLY host this Program on github, you cant find it somewhere else! ( You can, but only if some B4st4rd uploaded it there. )
 echo [NOTE] You CANT use programs such as diskpart , because they need administrative rights!
 echo [NOTE] Press any key to get to set up B4TCHPL01T
 pause > nul
-
+cls
 color 0a
 echo.
 echo.
@@ -72,16 +79,16 @@ echo [SUCCESS]
 cls
 echo "[BPL01TSID] Your B4TCHPL01T SID: %random%_%random%_B4TCHPL01T_%random%" > B4TCHPL01TSID.txt
 cls
-echo  ▄▄▄▄    ▄▄▄     ▄▄▄█████▓ ▄████▄   ██░ ██  ██▓███   ██▓     ▒█████   ██▓▄▄▄█████▓
-echo ▓█████▄ ▒████▄   ▓  ██▒ ▓▒▒██▀ ▀█  ▓██░ ██▒▓██░  ██▒▓██▒    ▒██▒  ██▒▓██▒▓  ██▒ ▓▒
-echo ▒██▒ ▄██▒██  ▀█▄ ▒ ▓██░ ▒░▒▓█    ▄ ▒██▀▀██░▓██░ ██▓▒▒██░    ▒██░  ██▒▒██▒▒ ▓██░ ▒░
-echo ▒██░█▀  ░██▄▄▄▄██░ ▓██▓ ░ ▒▓▓▄ ▄██▒░▓█ ░██ ▒██▄█▓▒ ▒▒██░    ▒██   ██░░██░░ ▓██▓ ░ 
-echo ░▓█  ▀█▓ ▓█   ▓██▒ ▒██▒ ░ ▒ ▓███▀ ░░▓█▒░██▓▒██▒ ░  ░░██████▒░ ████▓▒░░██░  ▒██▒ ░ 
-echo ░▒▓███▀▒ ▒▒   ▓▒█░ ▒ ░░   ░ ░▒ ▒  ░ ▒ ░░▒░▒▒▓▒░ ░  ░░ ▒░▓  ░░ ▒░▒░▒░ ░▓    ▒ ░░   
-echo ▒░▒   ░   ▒   ▒▒ ░   ░      ░  ▒    ▒ ░▒░ ░░▒ ░     ░ ░ ▒  ░  ░ ▒ ▒░  ▒ ░    ░    
-echo  ░    ░   ░   ▒    ░      ░         ░  ░░ ░░░         ░ ░   ░ ░ ░ ▒   ▒ ░  ░      
-echo  ░            ░  ░        ░ ░       ░  ░  ░             ░  ░    ░ ░   ░           
-echo       ░                   ░                                                         
+
+echo ██████╗   ██╗██╗████████╗ █████╗ ██╗  ██╗██████╗ ██╗      █████╗   ███╗  ████████╗
+echo ██╔══██╗ ██╔╝██║╚══██╔══╝██╔══██╗██║  ██║██╔══██╗██║     ██╔══██╗ ████║  ╚══██╔══╝
+echo ██████╦╝██╔╝ ██║   ██║   ██║  ╚═╝███████║██████╔╝██║     ██║  ██║██╔██║     ██║   
+echo ██╔══██╗███████║   ██║   ██║  ██╗██╔══██║██╔═══╝ ██║     ██║  ██║╚═╝██║     ██║   
+echo ██████╦╝╚════██║   ██║   ╚█████╔╝██║  ██║██║     ███████╗╚█████╔╝███████╗   ██║   
+echo ╚═════╝      ╚═╝   ╚═╝    ╚════╝ ╚═╝  ╚═╝╚═╝     ╚══════╝ ╚════╝ ╚══════╝   ╚═╝   
+echo.
+echo [VERSION] A.1.0.1
+
 
 echo.
 echo. 
